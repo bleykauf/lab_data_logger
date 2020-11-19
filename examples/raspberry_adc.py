8b695da9dda6ca2ff96b26d5a73f2c0392a40fd5
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2020-11-19 14:36:20 +0100

    Add example service for RaspberryPi ADC. Closes #3.

---
+++ examples/raspberry_adc.py
@@ -0,0 +1,43 @@
+from lab_data_logger.services import LabDataService
+
+import pipyadc.ADS1256_default_config
+from pipyadc import ADS1256
+from pipyadc.ADS1256_definitions import (
+    NEG_AINCOM,
+    POS_AIN0,
+    POS_AIN1,
+    POS_AIN2,
+    POS_AIN3,
+)
+
+
+class PiPyADCService(LabDataService):
+    """
+    Provides pressure readings from a vacuum gauge, read in via PiPyADC.
+
+
+    Data Service to read a monitor voltage of a pressure sensor and convert that value
+    to a pressure value.
+    """
+
+    def __init__(self):
+        super(PiPyADCService, self).__init__()
+
+        POTI, LDR, EXT2, EXT3 = (
+            POS_AIN0 | NEG_AINCOM,
+            POS_AIN1 | NEG_AINCOM,
+            POS_AIN2 | NEG_AINCOM,
+            POS_AIN3 | NEG_AINCOM,
+        )
+        self.CH_SEQUENCE = (POTI, LDR, EXT2, EXT3)
+
+        self.ads = ADS1256(pipyadc.ADS1256_default_config)
+        self.ads.cal_self()
+
+    def exposed_get_data(self, fields=None):
+        raw_channels = self.ads.read_sequence(self.CH_SEQUENCE)
+        voltages = [i * self.ads.v_per_digit for i in raw_channels]
+
+        data = {"fields": {"pressure_sensor": 10 ** (5 / 7 * voltages[3] - 10)}}
+
+        return data
