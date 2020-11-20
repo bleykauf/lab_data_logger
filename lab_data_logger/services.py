a205572ada55522facfdd66027100641a76c34fc
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2020-11-20 14:53:31 +0100

    Fix imports from current working directory. Fixes #7.

--- lab_data_logger/services.py
+++ lab_data_logger/services.py
@@ -6,9 +6,10 @@ These are the objects that provide the data that we want to log.
 
 import importlib
 import os
+import random
+import sys
 from multiprocessing import Process
 
-import random
 import rpyc
 
 # multiprocessing needs pickling
@@ -73,21 +74,12 @@ def start_service(service, port):
     """
     service_name = service.split(".")[-1]
     module_name = service[: -len(service_name) - 1]
-    try:
-        print("Trying to start {} from {}".format(service_name, module_name))
-        module = importlib.import_module(module_name)
-    # This is a workaround for when the module that contains the service is in the
-    # current directory. In this case ModuleNotFoundError is raised for some reason.
-    except ModuleNotFoundError:
-        print("No module {} found".format(module_name))
-        path_to_file = os.path.join(os.getcwd(), *module_name.split(".")) + ".py"
-        print("Looking for {} in {}".format(service_name, path_to_file))
-        spec = importlib.util.spec_from_file_location(module_name, path_to_file)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
+    # add working directory to PATH, to allow to importing modules from there
+    sys.path.append(os.getcwd())
+    module = importlib.import_module(module_name)
     service = getattr(module, service_name)
     threaded_server = rpyc.utils.server.ThreadedServer(service, port=int(port))
 
     proc = Process(target=threaded_server.start)
     proc.start()
-    print("Started {} on port {}.".format(service_name, port))
+    print("Started {} on port {}.".format(service, port))
